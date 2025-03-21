36d1d5215007ef515d706505ddf08c499d5b634c
JoaoBornelli <joao.bornelli@edu.sc.senai.br>
2025-03-21 15:41:00 -0300

    update - refactored connection to broker and created callback to listen to messages incoming

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 
-// put function declarations here:
 Servo servo;
 
 const char* ssid = "iPhone";
@@ -20,12 +19,12 @@ const char* topic = "servo";
 WiFiClient client;
 PubSubClient mqttClient(client);
 
+void callback(char* topic, byte* message, unsigned int length);
+void mqttReconnect();
+
 void setup() {
   servo.attach(20);
   Serial.begin(9600);
-
-  mqttClient.setServer(broker, port);
-  
   WiFi.begin(ssid, pass);
   Serial.println("Connecting to Wifi...");
   while (WiFi.status() != WL_CONNECTED) {
@@ -33,16 +32,48 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("Wifi Connected");
-
   mqttClient.setServer(broker, port);
-  mqttClient.connect("ESP32-Servo",mqtt_user,mqtt_pass);
+  mqttClient.setCallback(callback);
+  mqttReconnect();
+  mqttClient.subscribe(topic);
+
+ 
+}
+void mqttReconnect(){
   Serial.println("Connecting to the Broker...");
   while (!mqttClient.connected()) {
-    Serial.println(".");
+    if (mqttClient.connect("ESP32-Servo", mqtt_user, mqtt_pass)) {
+      Serial.println(" Conectado!");
+      mqttClient.subscribe(topic);
+    } else {
+      Serial.print(" Falha, código: ");
+      Serial.print(mqttClient.state());
+      Serial.println(". Tentando novamente em 5 segundos...");
+      delay(5000);
+    }
   }
-  Serial.println("Broker Connected");
 }
 
+
+void callback(char* topic, byte* payload, unsigned int length){
+  String message = "";
+  byte angle;
+  for (int i = 0; i < length; i++) {
+    char c = (char)payload[i];
+    message += c;
+  }
+  angle = message.toInt();
+  if((angle >= 0) && (angle <= 180)){
+    servo.write(angle);
+  }else{
+    Serial.println("Angulo inválido");
+  }
+}
+
+
 void loop() {
-  
+  if (!mqttClient.connected()) {
+    mqttReconnect();
+  }
+  mqttClient.loop();
 }
